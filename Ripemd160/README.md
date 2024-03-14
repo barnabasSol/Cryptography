06cec9b49e112e2d70e4d1ccfb1c562869b1853a
barnabas <xobarneysolomon@gmail.com>
2024-03-14 08:54:15 +0300

    initial

---
+++ Ripemd160/README.md
@@ -0,0 +1,5 @@
+# step 1
+
+## accept string
+
+insure it is of length of 512 bit and apply padding if its short | partition to 512 bit block if too long
---
+++ Ripemd160/Utils/Buffers.cs
@@ -0,0 +1,7 @@
+namespace Ripemd160.Utils;
+
+public static class InitVariables
+{
+    public static uint[] Buffers = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
+    public static uint[] BuffersPrime = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
+}
